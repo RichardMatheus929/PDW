0ea6bf4f7420540b4d8e3f84bd6de8cd1b33b339
Richard Matheus <richardmatheus929@gmail.com>
2023-09-28 19:43:00 -0300

    Desenvolvimento web

---
+++ Romano/romano.py
@@ -0,0 +1,19 @@
+﻿def converter(num):
+
+    valores = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+    simbolos = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+    resultado = ''
+    indice = 0 
+
+    while num > 0:
+
+        if num >= valores[indice]:
+            resultado += simbolos[indice]
+            num -= valores[indice] 
+        else:
+            indice += 1
+
+    return resultado
+
+
+print(converter(1250))
